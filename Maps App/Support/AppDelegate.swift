9cd5e129b4fd4afbad3a45380902b025f33bb09a
Colin Murphy <cmurphytaylor@gmail.com>
2020-10-26 10:52:00 -0400

    AppDelegate API Key

--- Maps App/Support/AppDelegate.swift
+++ Maps App/Support/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GMSPlacesClient.provideAPIKey("AIzaSyD5Pc99XpJeNjCaitz8nAzWiaHXvEN6By4")
-        GMSServices.provideAPIKey("AIzaSyD5Pc99XpJeNjCaitz8nAzWiaHXvEN6By4")
+        #warning("Insert API key here")
+        
         return true
     }
 
